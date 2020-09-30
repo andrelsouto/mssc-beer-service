7fe40112bc02f7185ba6298144dff63ba47d1008
André Luiz <andre.andrade@vsoft.com.br>
2020-09-30 15:35:34 -0300

    adding web data model. Closes #1

--- src/main/java/br/com/andre/msscbeerservice/web/model/BeerPagedList.java
+++ src/main/java/br/com/andre/msscbeerservice/web/model/BeerPagedList.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public class BeerPagedList extends PageImpl<BeerDto> {
+
     public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
         super(content, pageable, total);
     }
