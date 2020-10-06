a9bfb0f705ce0f98040c7c2dd8109dce94d3ce27
André Luiz <andre.andrade@vsoft.com.br>
2020-10-06 07:59:29 -0300

    Adding libs of beers. Closes #18

---
+++ target/generated-sources/annotations/br/com/andre/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,15 @@
+package br.com.andre.msscbeerservice.web.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-10-06T07:30:38-0300",
+    comments = "version: 1.4.0.Final, compiler: javac, environment: Java 11.0.8 (Oracle Corporation)"
+)
+@Component
+@Primary
+public class BeerMapperImpl extends BeerMapperDecorator implements BeerMapper {
+}
