29fd09c3cefcced918a171cb3945a4274f9efd45
André Luiz <andre.andrade@vsoft.com.br>
2020-11-02 17:28:35 -0300

    Configuring local mysql spring profile.

--- src/main/java/br/com/andre/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/br/com/andre/msscbeerservice/bootstrap/BeerLoader.java
@@ -2,13 +2,16 @@ package br.com.andre.msscbeerservice.bootstrap;
 
 import br.com.andre.msscbeerservice.domain.Beer;
 import br.com.andre.msscbeerservice.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.UUID;
 
-//@Component
+@RequiredArgsConstructor
+@Component
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
@@ -20,10 +23,6 @@ public class BeerLoader implements CommandLineRunner {
 
     private final BeerRepository beerRepository;
 
-    public BeerLoader(BeerRepository beerRepository) {
-        this.beerRepository = beerRepository;
-    }
-
     @Override
     public void run(String... args) throws Exception {
         loadBeerObjects();
@@ -31,29 +30,31 @@ public class BeerLoader implements CommandLineRunner {
 
     private void loadBeerObjects() {
         if (beerRepository.count() == 0) {
-            beerRepository.save(Beer.builder()
+            Beer b1 = Beer.builder()
                     .beerName("Mango Bobs")
                     .beerStyle("IPA")
                     .quantityToBrew(200)
                     .minOnHand(12)
                     .upc(BEER_1_UPC)
-                    .price(new BigDecimal("12.95")).build());
+                    .price(new BigDecimal("12.95")).build();
 
-            beerRepository.save(Beer.builder()
+            Beer b2 = Beer.builder()
                     .beerName("Galaxy Cat")
                     .beerStyle("PALE_ALE")
                     .quantityToBrew(200)
                     .minOnHand(12)
                     .upc(BEER_2_UPC)
-                    .price(new BigDecimal("11.95")).build());
+                    .price(new BigDecimal("11.95")).build();
 
-            beerRepository.save(Beer.builder()
-                    .beerName("No Hammers On The Bar")
+            Beer b3 = Beer.builder()
+                    .beerName("No Hamme rs On The Bar")
                     .beerStyle("PALE_ALE")
                     .quantityToBrew(200)
                     .minOnHand(12)
                     .upc(BEER_3_UPC)
-                    .price(new BigDecimal("11.95")).build());
+                    .price(new BigDecimal("11.95")).build();
+
+            beerRepository.saveAll(Arrays.asList(b1, b2, b3));
         }
     }
 }
