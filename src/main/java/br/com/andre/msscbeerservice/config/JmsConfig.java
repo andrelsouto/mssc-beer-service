8e6b39db8e4fdf4595c71ca43c81090c7dca68a4
André Luiz <andre.andrade@vsoft.com.br>
2020-11-04 20:53:52 -0300

    Fixing Jackson Configuration. Closes #29

--- src/main/java/br/com/andre/msscbeerservice/config/JmsConfig.java
+++ src/main/java/br/com/andre/msscbeerservice/config/JmsConfig.java
@@ -1,11 +1,15 @@
 package br.com.andre.msscbeerservice.config;
 
+import br.com.andre.msscbeerservice.events.NewInventoryEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class JmsConfig {
@@ -17,7 +21,10 @@ public class JmsConfig {
     public MessageConverter jacksonJmsConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
-        converter.setTypeIdPropertyName("_type");
+        Map<String, Class<?>> typeIdMappings = new HashMap<String, Class<?>>();
+        typeIdMappings.put("JMS_TYPE", NewInventoryEvent.class);
+        converter.setTypeIdMappings(typeIdMappings);
+        converter.setTypeIdPropertyName("JMS_TYPE");
         converter.setObjectMapper(objectMapper);
         return converter;
     }
