c39d3ed2bd95bd69e0109e4e770734d3a235f7a0
André Luiz <andre.andrade@vsoft.com.br>
2021-01-26 20:57:39 -0300

    Configuring Eureka Client. Closes #31

---
+++ src/main/java/br/com/andre/msscbeerservice/config/LocalDiscovery.java
@@ -0,0 +1,11 @@
+package br.com.andre.msscbeerservice.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("local-discovery")
+@EnableDiscoveryClient
+@Configuration
+public class LocalDiscovery {
+}
