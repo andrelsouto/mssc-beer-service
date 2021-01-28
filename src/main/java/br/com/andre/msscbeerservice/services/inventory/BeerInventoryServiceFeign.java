2804ca2a4fc43bb61bdfb355e8f1b480cb576a8b
André Luiz <andre.andrade@vsoft.com.br>
2021-01-28 06:18:31 -0300

    Ident

--- src/main/java/br/com/andre/msscbeerservice/services/inventory/BeerInventoryServiceFeign.java
+++ src/main/java/br/com/andre/msscbeerservice/services/inventory/BeerInventoryServiceFeign.java
@@ -33,7 +33,7 @@ public class BeerInventoryServiceFeign implements BeerInventoryService {
                 .mapToInt(BeerInventoryDto::getQuantityOnHand)
                 .sum();
 
-        log.debug("BeerId: " + beerId +  " On hand is: " + onHand);
+        log.debug("BeerId: " + beerId + " On hand is: " + onHand);
 
         return onHand;
 
