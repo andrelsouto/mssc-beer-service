87f36295bcc4ca9866d97d3048de5b1d9e8645c9
André Luiz <andre.andrade@vsoft.com.br>
2021-03-22 08:29:05 -0300

    Configuring Spring Cloud Sleuth and Zipkin

--- src/main/java/br/com/andre/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/br/com/andre/msscbeerservice/MsscBeerServiceApplication.java
@@ -3,8 +3,10 @@ package br.com.andre.msscbeerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableFeignClients
+@EnableJpaRepositories
 @SpringBootApplication
 public class MsscBeerServiceApplication {
 
