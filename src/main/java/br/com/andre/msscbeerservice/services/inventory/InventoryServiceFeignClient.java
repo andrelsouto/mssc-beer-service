b4a27d8163a2d457caabc4096871a13ff329c38d
André Souto <a.luiz.souto@gmail.com>
2021-04-12 11:54:33 -0300

    Adding Credentials for Beer Inventory Service Request

--- src/main/java/br/com/andre/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
+++ src/main/java/br/com/andre/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
@@ -1,5 +1,6 @@
 package br.com.andre.msscbeerservice.services.inventory;
 
+import br.com.andre.msscbeerservice.config.FeignClientConfig;
 import br.com.andre.msscbeerservice.services.inventory.model.BeerInventoryDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(name = "beer-inventory-service", fallback = InventoryServiceFeignClientFailover.class)
+@FeignClient(name = "beer-inventory-service", fallback = InventoryServiceFeignClientFailover.class, configuration = FeignClientConfig.class)
 public interface InventoryServiceFeignClient {
 
     @RequestMapping(method = RequestMethod.GET, value = BeerInventoryServiceRestTemplateImpl.INVENTORY_PATH)
